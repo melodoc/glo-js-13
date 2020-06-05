5c1b624dc01ebfa12d0e12d88b6b32c043a26793
melodoc <l_tih@ro.ru>
2020-06-05 18:39:46 +0300

    Add new variables

--- script.js
+++ script.js
@@ -5,8 +5,26 @@ let deposit;
 let mission;
 let period;
 
-let message;
+money = 5000;
+income = 'freelance';
+addExpenses = 'Internet, transport, service';
+deposit = true;
+mission = 100000;
+period = 12;
 
-message = "Сообщение с любым текстом";
-alert(message);
-console.log(message);
+console.log('data type of variable "money": ' + typeof money);
+console.log('data type of variable "income": ' + typeof income);
+console.log('data type of variable "deposit": ' + typeof deposit);
+console.log('length of string "addExpenses": ' + addExpenses.length);
+console.log(`The period is ${period} months`);
+console.log(`The goal is earning ${mission} USD`);
+
+addExpenses = addExpenses.toLocaleLowerCase();
+addExpenses = addExpenses.split(', ');
+
+console.log(addExpenses);
+
+let budgetDay;
+budgetDay = money / 30;
+
+console.log(budgetDay.toFixed(2));
