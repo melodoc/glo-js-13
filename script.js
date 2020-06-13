8698e256508d63b589c6c4252f27d0c4e4ecc67f
melodoc <l_tih@ro.ru>
2020-06-13 13:20:41 +0300

    Add solution

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+let isNumber = function (number) {
+    return !isNaN(parseFloat(number)) && isFinite(number);
+};
+
+let checkQuizInput = function () {
+    let quizInput = +prompt('Угадай чисто от 1 до 100', '100');
+    let quizNumber = 55;
+
+    if (quizInput === quizNumber) {
+        confirm('Вы угадали число');
+        return;
+    }
+
+    if (!isNumber(quizInput)) {
+        alert('Вы ввели не число');
+    }
+
+    if (quizInput > quizNumber) {
+        alert('Загаданное число меньше');
+    }
+
+    if (quizInput < quizNumber) {
+        alert('Загаданное число больше');
+    }
+    checkQuizInput();
+};
+
+checkQuizInput();
