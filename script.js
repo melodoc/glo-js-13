867387dafe60db49f89d373b0c107659f61601bb
melodoc <l_tih@ro.ru>
2020-07-15 01:57:43 +0300

    Add solution

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const date = new Date();
+
+weekDays.forEach((day, dayNumber) => {
+    const outputDay = document.createElement('p');
+    outputDay.innerHTML = day;
+    document.body.appendChild(outputDay);
+
+    if (dayNumber === 5 || dayNumber === 6) {
+        outputDay.style.fontStyle = 'italic';
+    }
+    let currentDay = date.getDay();
+    if (currentDay === 0) {
+        currentDay = 7;
+    }
+    if (dayNumber === (currentDay - 1)) {
+        outputDay.style.fontWeight = 'bold';
+    }
+});
